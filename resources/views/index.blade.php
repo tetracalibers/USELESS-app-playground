986f8efb781151127459d88df9dbf02f4e4b3c29
tetracalibers <tomix05may@gmail.com>
2022-03-09 17:44:34 +0900

    CSSファイルパスをcss/から/css/に変更

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,11 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>tomixy's playground</title>
-    <link rel="stylesheet" href="css/destyle.css" />
-    <link rel="stylesheet" href="css/materialize.min.css">
-    <link rel="stylesheet" href="css/materialize-icon.css"/>
-    <link rel="stylesheet" href="css/skeleton.css">
-    <link rel="stylesheet" href=" {{ mix('css/styles.css') }}">
+    <link rel="stylesheet" href="/css/destyle.css" />
+    <link rel="stylesheet" href="/css/materialize.min.css">
+    <link rel="stylesheet" href="/css/materialize-icon.css"/>
+    <link rel="stylesheet" href="/css/skeleton.css">
+    <link rel="stylesheet" href="{{ mix('css/styles.css') }}">
     <!-- 3d-react-carousal用アイコン -->
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
     <script src="{{ mix('js/index.js') }}" defer="defer"></script>
