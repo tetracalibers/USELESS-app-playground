c7804df39600decc2b8a2ee900e998b6d03a9eec
tetracalibers <tomix05may@gmail.com>
2022-03-11 08:31:41 +0900

    SafariでSVG画像が表示されない問題へ対処中

--- resources/frontend/app/tpusg/components/website/atoms/Clock.jsx
+++ resources/frontend/app/tpusg/components/website/atoms/Clock.jsx
@@ -1,19 +1,20 @@
-import React from "react";
-import { css } from "@emotion/css";
-import { useWindowSize } from "@react-hook/window-size";
-//import clockSVG from '../../../../../../../public/images/tpusg/tpusgClockTop.svg'
+import React from 'react'
+import { css } from '@emotion/css'
+import { useWindowSize } from '@react-hook/window-size'
 
 export default function Clock() {
-    const [width, height] = useWindowSize();
-    const clockSVG = "../images/tpusg/tpusgClockTop.svg";
-    const css_svg = css`
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        width: ${width < height ? "100vw" : "auto"};
-        height: ${width > height ? "100vh" : "auto"};
-    `;
+  const [width, height] = useWindowSize()
+  const clockSVG = '../images/tpusg/tpusgClockTop.svg'
+  const css_svg = css`
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: ${width < height ? '100vw' : 'auto'};
+    height: ${width > height ? '100vh' : 'auto'};
+  `
 
-    return <img src={clockSVG} className={css_svg} />;
+  return (
+    <object type="image/svg+xml" data={clockSVG} className={css_svg}></object>
+  )
 }
