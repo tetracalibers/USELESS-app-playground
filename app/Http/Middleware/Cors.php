866b049e5fdd24d911dad55952915b20821671cf
tetracalibers <tomix05may@gmail.com>
2022-03-11 12:00:01 +0900

    APIレスポンスにCORS用のヘッダを付与

---
+++ app/Http/Middleware/Cors.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class Cors
+{
+    public function handle(Request $request, Closure $next)
+    {
+        // すべてのレスポンスに CORS 用のヘッダーを追加する必要はないので URL から判断する
+        $paths = explode('/', $request->getPathInfo());
+        if ($paths[1] === 'api') {
+            return $next($request)
+                ->header('Access-Control-Allow-Origin', '*')
+                ->header('Access-Control-Allow-Methods', 'GET, POST')
+                ->header('Access-Control-Allow-Headers', 'Accept, X-Requested-With, Origin, Content-Type');
+        }
+        return $next($request);
+    }
+}
